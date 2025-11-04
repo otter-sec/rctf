8abd40be872f4bf7ed133372d0e3e2818b146770
enscribe <jason@enscribe.dev>
2025-11-03 17:41:42 -0800

    fix: drizzle env variable

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -5,7 +5,11 @@ export default defineConfig({
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url:
+      process.env.RCTF_DATABASE_URL ??
+      (() => {
+        throw new Error('RCTF_DATABASE_URL must be set for Drizzle commands')
+      })(),
   },
   verbose: true,
   strict: true,
