be6c91c0a7f95c1c1e1b66756bca4c0fc3854ace
es3n1n <git@es3n1n.im>
2025-11-09 19:53:39 +0100

    style: minor web style changes

--- apps/web/svelte.config.ts
+++ apps/web/svelte.config.ts
@@ -1,8 +1,8 @@
 import adapter from '@sveltejs/adapter-auto'
+import type { Config } from '@sveltejs/kit'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+const config: Config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
   preprocess: vitePreprocess(),
@@ -12,6 +12,12 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter(),
+
+    typescript: {
+      config: cfg => {
+        cfg.extends = '../../../tsconfig.json'
+      },
+    },
   },
 }
 
