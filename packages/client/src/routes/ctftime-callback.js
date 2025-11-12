8bc1b882fdffaecf14a57ffe368705c6be55147d
es3n1n <git@es3n1n.im>
2025-11-12 16:18:16 +0100

    fix(ctftime): fix ctftime callback params

--- packages/client/src/routes/ctftime-callback.js
+++ packages/client/src/routes/ctftime-callback.js
@@ -2,11 +2,12 @@ import { Component } from 'preact'
 
 export default class CtftimeCallback extends Component {
   componentDidMount() {
+    const params = new URLSearchParams(location.search)
     window.opener.postMessage(
       {
         kind: 'ctftimeCallback',
-        state: this.props.state,
-        ctftimeCode: this.props.code,
+        state: params.get('state'),
+        ctftimeCode: params.get('code'),
       },
       location.origin
     )
