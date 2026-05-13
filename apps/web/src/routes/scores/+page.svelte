eba75753fe256881f0bb5aeee4e95dc00c3eb217
enscribe <jason@enscribe.dev>
2026-05-13 12:49:51 -0700

    refactor(web): unshittify

--- apps/web/src/routes/scores/+page.svelte
+++ apps/web/src/routes/scores/+page.svelte
@@ -1,17 +1,12 @@
 <script lang="ts">
-  import { CtfNotStarted, EmptyState, ScrollArea, Spinner, Tooltip } from '$lib/components'
+  import { CtfNotStarted, EmptyState, Spinner, Tooltip } from '$lib/components'
   import { IconChartAreaLineFilled, IconFlagFilled } from '$lib/icons'
-  import { cn, createHoverTooltip } from '$lib/utils'
+  import { createHoverTooltip } from '$lib/utils'
   import { getCategoryConfig } from '$lib/utils/categories'
-  import type { Attachment } from 'svelte/attachments'
   import ScoresCellTooltipContent from './scores-cell-tooltip-content.svelte'
-  import ScoresChallengeHeader from './scores-challenge-header.svelte'
   import { createScoresDataModel, createScoresGraphDataModel } from './scores-data-model.svelte'
-  import ScoresFades from './scores-fades.svelte'
-  import ScoresGraphControls from './scores-graph-controls.svelte'
-  import ScoresGraph from './scores-graph.svelte'
-  import { CELL_WIDTH, HEADER_HEIGHT, ROW_GAP, ROW_HEIGHT } from './scores-layout-constants'
   import ScoresLeaderboardBody from './scores-leaderboard-body.svelte'
+  import ScoresLeaderboardFrame from './scores-leaderboard-frame.svelte'
   import { createScoresRouteState } from './scores-route-state.svelte'
   import ScoresScreenshotModal from './scores-screenshot-modal.svelte'
   import ScoresToolbar from './scores-toolbar.svelte'
@@ -78,14 +73,6 @@
   const hoveredTeamId = $derived(sparklineHoveredTeamId ?? tooltipGraphState.hoveredTeamId)
   const solveHighlight = $derived(tooltipGraphState.solveHighlight)
 
-  const headerRowAttachment: Attachment<HTMLElement> = node => {
-    viewportState.headerRowRef = node
-    return () => {
-      if (viewportState.headerRowRef === node) viewportState.headerRowRef = null
-    }
-  }
-
-  const scroll = viewportState.scroll
   const focusedChallenge = $derived.by(() => {
     const id = routeState.focusedChallengeId
     const challenge = id ? scoreData.challengesData[id] : null
@@ -173,140 +160,24 @@
         />
       </div>
     {:else}
-      <div class="flex justify-center px-4 md:px-9">
-        <div
-          class="relative w-full max-w-full md:w-fit"
-          style:--row-height="{ROW_HEIGHT - ROW_GAP}px"
-          style:--row-height-full="{ROW_HEIGHT}px"
-          style:--cell-width="{CELL_WIDTH}px"
-          style:--header-height="{HEADER_HEIGHT}px"
-          style:--name-row-height="128px"
-          style:--diagonal-overflow="96px"
-          style:--team-column-width={viewportState.isDesktop
-            ? viewportState.isXl
-              ? 'calc(45vw - 72px)'
-              : 'calc(60vw - 72px)'
-            : '100%'}
-          style:--content-column-width={viewportState.isDesktop
-            ? viewportState.isXl
-              ? 'calc(55vw + 72px)'
-              : 'calc(40vw + 72px)'
-            : '0px'}
-          style:--self-row-height="{ROW_HEIGHT}px"
-          style:--self-row-offset={viewportState.showSelfRow &&
-          viewportState.selfRowPosition === 'bottom'
-            ? `${ROW_HEIGHT}px`
-            : '0px'}
-          style:--self-row-top-offset={viewportState.showSelfRow &&
-          viewportState.selfRowPosition === 'top'
-            ? `${ROW_HEIGHT}px`
-            : '0px'}
-          style:--score-scroll-padding-top={viewportState.showSelfRow &&
-          viewportState.selfRowPosition === 'top'
-            ? viewportState.isDesktop
-              ? 'calc(var(--header-height) + var(--row-height-full) + 4px)'
-              : 'calc(var(--row-height-full) + 4px)'
-            : viewportState.isDesktop
-              ? 'var(--header-height)'
-              : '0px'}
-        >
-          <ScoresFades
-            showTop={viewportState.showTopFade}
-            showBottom={viewportState.showBottomFade}
-            showLeft={viewportState.isDesktop && viewportState.showLeftFade}
-            showRight={viewportState.isDesktop && viewportState.showRightFade}
-            showSelfRow={viewportState.showSelfRow}
-            selfRowPosition={viewportState.selfRowPosition}
-            isMinimal={!viewportState.isDesktop}
-          />
-
-          <div
-            class="
-              group/graph bg-background-l1 relative mb-2 h-(--header-height) rounded-lg
-              md:hidden
-            "
-          >
-            <ScoresGraphControls
-              showTop3Context={routeState.showTop3Context}
-              showSelfContext={routeState.showSelfContext}
-              onShowTop3ContextChange={routeState.setShowTop3Context}
-              onShowSelfContextChange={routeState.setShowSelfContext}
-            />
-            <ScoresGraph class="h-full w-full p-3" {...graphProps} />
-          </div>
-
-          <ScrollArea
-            class={viewportState.isDesktop
-              ? 'h-[calc(100dvh-72px-52px-16px)]'
-              : 'h-[calc(100dvh-72px-96px-8px-192px-16px)]'}
-            orientation={viewportState.isDesktop ? 'both' : 'vertical'}
-            type={viewportState.isDesktop ? 'always' : 'auto'}
-            fadeSize={0}
-            bind:viewportRef={scroll.state.viewportRef}
-            viewportTabIndex={-1}
-            viewportClass={cn(
-              'scroll-pt-(--score-scroll-padding-top)',
-              viewportState.isDesktop && 'scroll-pl-(--team-column-width)'
-            )}
-            scrollbarXClasses={viewportState.isDesktop
-              ? 'pl-(--team-column-width) -mr-2.5 z-40'
-              : 'hidden'}
-            scrollbarYClasses={`z-40 ${viewportState.scrollbarYPadding}`}
-          >
-            <div class="flex min-h-full flex-col">
-              <div
-                class="bg-background-l0 sticky top-0 z-20 hidden h-(--header-height) md:flex"
-                {@attach headerRowAttachment}
-              >
-                <div
-                  class="
-                    group/graph bg-background-l0 sticky left-0 z-30 w-(--team-column-width)
-                    shrink-0
-                  "
-                >
-                  <div class="bg-background-l1 h-full w-full rounded-t-3xl rounded-bl-xl">
-                    <ScoresGraphControls
-                      showTop3Context={routeState.showTop3Context}
-                      showSelfContext={routeState.showSelfContext}
-                      onShowTop3ContextChange={routeState.setShowTop3Context}
-                      onShowSelfContextChange={routeState.setShowSelfContext}
-                    />
-                    <ScoresGraph class="h-full w-full p-3" {...graphProps} />
-                  </div>
-                </div>
-                {#if !scoreData.challengesQuery.isLoading}
-                  <ScoresChallengeHeader
-                    viewMode={routeState.viewMode}
-                    sortMode={routeState.sortMode}
-                    categoryGroups={scoreData.categoryGroups}
-                    {challenges}
-                    focusedChallengeId={routeState.focusedChallengeId}
-                    onChallengeFocus={id => {
-                      const wasFocused = routeState.focusedChallengeId === id
-                      routeState.setFocusedChallenge(wasFocused ? null : id)
-                      if (!wasFocused) {
-                        const viewportRef = scroll.state.viewportRef
-                        if (viewportRef) viewportRef.scrollTop = 0
-                      }
-                    }}
-                  />
-                {/if}
-              </div>
-
-              <ScoresLeaderboardBody
-                {scoreData}
-                {graphState}
-                {routeState}
-                {viewportState}
-                {challenges}
-                {renderEpoch}
-                onCellHover={cellTooltip.hover}
-                onSparklineHover={teamId => (sparklineHoveredTeamId = teamId)}
-              />
-            </div>
-          </ScrollArea>
-        </div>
-      </div>
+      <ScoresLeaderboardFrame
+        {scoreData}
+        {routeState}
+        {viewportState}
+        {graphProps}
+        {challenges}
+      >
+        <ScoresLeaderboardBody
+          {scoreData}
+          {graphState}
+          {routeState}
+          {viewportState}
+          {challenges}
+          {renderEpoch}
+          onCellHover={cellTooltip.hover}
+          onSparklineHover={teamId => (sparklineHoveredTeamId = teamId)}
+        />
+      </ScoresLeaderboardFrame>
     {/if}
   {/if}
 
