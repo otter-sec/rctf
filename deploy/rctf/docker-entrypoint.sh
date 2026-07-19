f2d4765c9b53942ba31ac0dbb9b4c9231ad3ffc0
es3n1n <git@es3n1n.im>
2026-07-19 20:44:29 +0200

    refactor: run api as 1001:1001

--- deploy/rctf/docker-entrypoint.sh
+++ deploy/rctf/docker-entrypoint.sh
@@ -4,7 +4,12 @@ set -eu
 mkdir -p /tmp/nginx
 chmod 0755 /tmp/nginx
 
+# v1 compatibility (forward only)
+if [ -d /app/uploads ]; then
+    chown -R rctf:rctf /app/uploads
+fi
+
 rctf deployment generate-csp --web-build /app/static > /tmp/nginx/security-headers.conf
 nginx -t -e stderr
 
-supervisord -c /etc/supervisord.conf
+exec supervisord -c /etc/supervisord.conf
