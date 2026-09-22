b762d31ebf5a8e2105eff00ea7b15ac89c1295a9
Victor <virb3e@gmail.com>
2026-09-22 17:47:09 +0100

    fix(api): require non-empty hostPath.path

--- apps/api/src/providers/instancer/k8s-instancer.ts
+++ apps/api/src/providers/instancer/k8s-instancer.ts
@@ -361,7 +361,7 @@ const volumeSchema = z.catchall(
       .optional(
         z.catchall(
           z.object({
-            path: z.string().check(z.describe('Path')),
+            path: z.string().check(z.minLength(1), z.describe('Path')),
             // https://github.com/kubernetes/api/blob/13152125c196531c20cae818abd3791701da6d80/core/v1/types.go#L900-L922
             type: z
               .optional(
